90c9b0f442719083919c9dcc1769813441be5329
Raphael DAmico <raph.damico@gmail.com>
2011-01-28 16:48:03 -0600

    changed everything in renamedfile copy

--- renamedFile copy.js
+++ renamedFile copy.js
@@ -1,10 +1,1 @@
-Test of a new javascript file
-
-More changes
-
-More and more and more changes.
-
-AND A HUGE FUCKUP1!!!!!!!
-
-
-Mega big changes to this file.
+Changed just about everything about this file
